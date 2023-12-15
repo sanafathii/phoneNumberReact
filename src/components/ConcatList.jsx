056f7cf727517e3a70d1f6a320aca603c50b7732
sana <sana.fathi30@gmail.com>
2023-12-15 17:21:03 +0330

    added sort func and search input

--- src/components/ConcatList.jsx
+++ src/components/ConcatList.jsx
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Input from "./Input";
 
 function ConcatList({ concatList, removeConcatHandler, editHandler }) {
+  const [allConcat, setAllConcat] = useState(concatList);
+  const [searchConcat, setSearchConcat] = useState("");
+
+  const changeHandeler = (e) => {
+    setSearchConcat(e.target.value);
+  };
+
+  useEffect(() => {
+    if (searchConcat) {
+      const searchedList = allConcat.filter((item) =>
+        item.concatName.toLowerCase().includes(searchConcat.toLowerCase())
+      );
+      setAllConcat(searchedList);
+    } else {
+      setAllConcat(concatList);
+    }
+  }, [searchConcat, concatList]);
+
+  const sortHandler = () => {
+    const sortedList = [...allConcat];
+
+    sortedList.sort((a, b) => a.concatName.localeCompare(b.concatName));
+
+    setAllConcat(sortedList);
+  };
+
   return (
     <div>
+      <Input
+        className=""
+        type="text"
+        name="searchConcat"
+        value={searchConcat}
+        placeholder="search concat"
+        onChange={changeHandeler}
+      />
+
+      <button onClick={sortHandler}>sotted Concat</button>
       <p>
-        {concatList.map((item) => {
+        {allConcat.map((item) => {
           return (
             <div key={item.id}>
               <p>
